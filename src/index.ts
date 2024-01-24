8575c6ca825a6ab1a26f2972e145bf71806c5fb9
nhaar <38634785+nhaar@users.noreply.github.com>
2024-01-24 12:54:37 -0300

    add extremely simple base video sharing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import path = require('path')
+import http = require('http')
 
 import express = require('express')
 import { Request, Response } from 'express'
 import cors = require('cors')
+import { Server } from 'socket.io'
 
 import Database from './database/database'
 import api from './api/api'
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
 
 if (process.env.NODE_ENV === 'dev') {
   app.use(cors())
@@ -21,11 +30,24 @@ app.use('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id)
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded')
+  })
+  socket.on('callUser', ({ userToCall, signalData, from, name}) => {
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name })
+  })
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal)
+  })
+})
+
 const db = new Database()
 db.initTables()
 
 const PORT = 5000
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
