0df71d5e7902a261e730359a8e69523715f7b191
nhaar <38634785+nhaar@users.noreply.github.com>
2024-01-30 11:58:27 -0300

    add basic screensharing via websocket and media recording

--- src/UserPage.tsx
+++ src/UserPage.tsx
@@ -1,16 +1,52 @@
-import VideoPlayer from './VideoPlayer'
-import { SocketContextProvider } from './Context/VideoContext'
+import { io, Socket } from 'socket.io-client'
+import { useEffect, useState } from 'react'
+import { SERVER_URL } from './urls'
 
 /**
  * Handles the page where non admin players can perform actions. Not to be confused with PlayerPage which handles all types of users
  * @returns
  */
 export default function UserPage (): JSX.Element {
+  /** WebSocket connection as a player */
+  const [socket, setSocket] = useState<Socket | null>(null)
+
+  /**
+   * Creates a media recorder that will send video chunks to the backend every 5 seconds
+   * @param stream 
+   */
+  function createMediaRecorder(stream: MediaStream) {
+    const mediaRecorder = new MediaRecorder(stream)
+    mediaRecorder.ondataavailable = (e) => {
+      if (e.data.size > 0) {
+        socket?.emit('message', { blob: e.data, type: e.data.type })
+      }
+      mediaRecorder.stop()
+      // because of how MediaRecorder works, the only way to have a readable video chunk is to record
+      // so recursively create new and disable old one.
+      mediaRecorder.ondataavailable = null
+      createMediaRecorder(stream)
+    }
+    mediaRecorder.start(5000)
+  }
+
+  /**
+   * Starts sharing screen to backend
+   */
+  function startScreensharing() {
+    navigator.mediaDevices.getDisplayMedia({ video: true, audio: true }).then((stream) => {
+      createMediaRecorder(stream)
+    })
+  }
+
+  // connecting socket
+  useEffect(() => {
+    const socket = io(SERVER_URL)
+    setSocket(socket)
+  }, [])  
+  
   return (
-    <SocketContextProvider>
-      <div>
-        <VideoPlayer />
-      </div>
-    </SocketContextProvider>
+    <div>
+      <button onClick={startScreensharing}>share</button>
+    </div>
   )
 }
